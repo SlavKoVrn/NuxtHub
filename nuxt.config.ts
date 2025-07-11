a4e930cade64d41224b44502d64bf723c652210f
SlavKoVrn <slavko.chita@gmail.com>
2025-07-11 18:30:31 +0300

    bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,21 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/test-utils']
+  modules: ['@nuxt/ui', '@nuxt/test-utils'],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
+        }
+      ],
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
+          defer: true
+        }
+      ]
+    }
+  }
 })
